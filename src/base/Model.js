22fb8b0e477ef0960afc25dab7a3ba5d77ae67d5
Mx <1318892153@qq.com>
2022-04-28 16:20:30 +0800

    重构 mvc 变成 model + view

---
+++ src/base/Model.js
@@ -0,0 +1,27 @@
+class Model {
+  constructor(options) {
+    ["data", "update", "create", "delete", "get"].forEach((key) => {
+      if (key in options) {
+        this[key] = options[key];
+      }
+    });
+  }
+  //增
+  create() {
+    console && console.error && console.error("你还没有实现 create");
+  }
+  //删
+  delete() {
+    console && console.error && console.error("你还没有实现 delete");
+  }
+  //改
+  update() {
+    console && console.error && console.error("你还没有实现 update");
+  }
+  //查
+  get() {
+    console && console.error && console.error("你还没有实现 get");
+  }
+}
+
+export default Model;
