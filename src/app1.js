f65ff0d1f246b4415742ebb6824e23d29bc9f9be
Mx <1318892153@qq.com>
2022-04-28 18:03:55 +0800

    完成了 EventBus 的继承

--- src/app1.js
+++ src/app1.js
@@ -1,25 +1,28 @@
 import "./app1.css";
 import $ from "jquery";
 import Model from "./base/Model.js";
+import View from "./base/View";
+import EventBus from "./base/EventBus";
 
-const eventBus = $(window);
 //数据相关 都放到m
 const m = new Model({
   data: {
-    n: parseInt(localStorage.getItem("n")),
+    n: parseFloat(localStorage.getItem("n")),
   },
   update: function (data) {
     Object.assign(m.data, data);
-    eventBus.trigger("m:updated");
+    m.trigger("m:updated");
     localStorage.setItem("n", m.data.n);
   },
 });
 
 //视图相关 都放到v
 //其他 都放到c
-const view = {
-  el: null,
-  html: `
+const init = (el) => {
+  new View({
+    el: el,
+    data: m.data,
+    html: `
       <div>
             <div class="output">
                 <span id="number">{{n}}</span>
@@ -31,46 +34,31 @@ const view = {
                 <button id="divide2">÷2</button>
             </div>
       </div>
-`,
-  init(container) {
-    view.el = $(container);
-    view.render(m.data.n); //view = render（data）
-    view.autoBindEvents();
-    eventBus.on("m:updated", () => {
-      view.render(m.data.n);
-    });
-  },
-  render(n) {
-    if (view.el.children.length !== 0) view.el.empty();
-    $(view.html.replace("{{n}}", n, m.data.n)).appendTo(view.el);
-  },
-  events: {
-    "click #add1": "add",
-    "click #minus1": "minus",
-    "click #mul2": "mul",
-    "click #divide2": "div",
-  },
-  add() {
-    m.update({ n: m.data.n + 1 });
-  },
-  minus() {
-    m.update({ n: m.data.n - 1 });
-  },
-  mul() {
-    m.update({ n: m.data.n * 2 });
-  },
-  div() {
-    m.update({ n: m.data.n / 2 });
-  },
-  autoBindEvents() {
-    for (let key in view.events) {
-      const value = view[view.events[key]];
-      const spaceIndex = key.indexOf(" ");
-      const part1 = key.slice(0, spaceIndex);
-      const part2 = key.slice(spaceIndex + 1);
-      view.el.on(part1, part2, value);
-    }
-  },
+     `,
+    render(data) {
+      const n = data.n;
+      if (this.el.children.length !== 0) this.el.empty();
+      $(this.html.replace("{{n}}", n, m.data.n)).appendTo(this.el);
+    },
+    events: {
+      "click #add1": "add",
+      "click #minus1": "minus",
+      "click #mul2": "mul",
+      "click #divide2": "div",
+    },
+    add() {
+      m.update({ n: m.data.n + 1 });
+    },
+    minus() {
+      m.update({ n: m.data.n - 1 });
+    },
+    mul() {
+      m.update({ n: m.data.n * 2 });
+    },
+    div() {
+      m.update({ n: m.data.n / 2 });
+    },
+  });
 };
 
-export default view;
+export default init;
