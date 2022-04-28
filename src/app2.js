e5e0e7cb778dc52606626c7f39792ef780b5d7d8
Mx <1318892153@qq.com>
2022-04-28 21:10:39 +0800

    MVC 转 Vue

--- src/app2.js
+++ src/app2.js
@@ -1,58 +1,24 @@
 import "./app2.css";
-import $ from "jquery";
-import Model from "./base/Model.js";
-import View from "./base/View";
-import EventBus from "./base/EventBus";
-
-const eventBus = new EventBus();
-
-const localKey = "app2.index";
-const m = new Model({
-  data: {
-    index: parseInt(localStorage.getItem(localKey)) || 0,
-  },
-  update(data) {
-    Object.assign(m.data, data);
-    eventBus.trigger("m:updated");
-    localStorage.setItem("app2.index", m.data.index);
-  },
-});
+import Vue from "vue";
 
 const init = (el) => {
-  new View({
+  new Vue({
     el: el,
-    data: m.data,
-    eventBus: eventBus,
-    html: (index) => {
-      return `
-      <div>
+    data: {
+      index: 0,
+    },
+    template: `
+    <section id="app2">
             <ol class="tab-bar">
-                <li class="${
-                  index === 0 ? "selected" : ""
-                }" data-index="0"><span>111</span></li>
-                <li class="${
-                  index === 1 ? "selected" : ""
-                }" data-index="1"><span>222</span></li>
+                <li :class="index ===0 ? 'selected' : ''" @click="index=0"><span>111</span></li>
+                <li :class="index ===1 ? 'selected' : ''" @click="index=1"><span>222</span></li>
             </ol>
             <ol class="tab-content">
-                <li class="${index === 0 ? "active" : ""}">内容1</li>
-                <li class="${index === 1 ? "active" : ""}">内容2</li>
+                <li :class="index ===0 ? 'active' : ''">内容1</li>
+                <li :class="index ===1 ? 'active' : ''">内容2</li>
             </ol>
-        </div>
-`;
-    },
-    render(data) {
-      const index = data.index;
-      if (this.el.children.length !== 0) this.el.empty();
-      $(this.html(index)).appendTo(this.el);
-    },
-    events: {
-      "click .tab-bar li": "x",
-    },
-    x(e) {
-      const index = parseInt(e.currentTarget.dataset.index);
-      m.update({ index: index });
-    },
+        </section>
+    `,
   });
 };
 
